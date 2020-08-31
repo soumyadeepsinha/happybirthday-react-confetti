9c8902f8147bb1ba39ee3d5ba975825d675efc29
soumyadepsinha <s.sinha2701@gmail.com>
2020-08-31 12:07:42 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Confetti from 'react-confetti'
+import './App.css'
+
+function App() {
+  const [height, setHeight] = useState(null)
+  const [width, setWidth] = useState(null)
+
+  const confettiRef = useRef(null)
+
+  useEffect(() => {
+    setHeight(confettiRef.current.clientHeight)
+    setWidth(confettiRef.current.clientWidth)
+  }, [])
+
+  return (
+    <React.Fragment>
+      <div className="main-div" ref={confettiRef}>
+        <div className="center_div">
+          <p>Wishing You a Very Happy Birthday Dear!
+            <span>🎂🍰</span>
+            <br />
+            <span className="love">❤</span> from Soumyadeep
+            <Confetti
+              numberOfPieces={250}
+              width={width}
+              height={height}
+            />
+          </p>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default App
